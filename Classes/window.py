0838595beb2ba51334d0a35d4737d9fb6750aec8
somePythonProgrammer <74598401+somePythonProgrammer@users.noreply.github.com>
2021-10-07 08:52:33 +0530

    Even cleaner code

---
+++ Classes/window.py
@@ -0,0 +1,65 @@
+class Window(pyglet.window.Window):
+
+    def push(self, pos, rot):
+        glPushMatrix()
+        rot = self.player.rot
+        pos = self.player.pos
+        glRotatef(-rot[0], 1, 0, 0)
+        glRotatef(-rot[1], 0, 1, 0)
+        glTranslatef(-pos[0], -pos[1], -pos[2])
+
+    def Projection(self):
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+
+    def Model(self):
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+
+    def set2d(self):
+        self.Projection()
+        gluPerspective(0, self.width, 0, self.height)
+        self.Model()
+
+    def set3d(self):
+        self.Projection()
+        gluPerspective(70, self.width/self.height, 0.05, 1000)
+        self.Model()
+
+    def setLock(self, state):
+        self.lock = state
+        self.set_exclusive_mouse(state)
+
+    lock = False
+    mouse_lock = property(lambda self: self.lock, setLock)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.set_minimum_size(800, 500)
+        self.keys = key.KeyStateHandler()
+        self.push_handlers(self.keys)
+        pyglet.clock.schedule(self.update)
+
+        self.model = Chunk()
+        self.player = Player((0.5, 1.5, 1.5), (-30, 0))
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        if self.mouse_lock:
+            self.player.mouse_motion(dx, dy)
+
+    def on_key_press(self, KEY, _MOD):
+        if KEY == key.ESCAPE:
+            self.close()
+        elif KEY == key.E:
+            self.mouse_lock = not self.mouse_lock
+
+    def update(self, dt):
+        self.player.update(dt, self.keys)
+
+    def on_draw(self):
+        self.clear()
+        self.set3d()
+        self.push(self.player.pos, self.player.rot)
+        self.model.draw()
+        glPopMatrix()
+        
