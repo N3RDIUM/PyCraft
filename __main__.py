a536a05647606f54756f2048dc65ab1388909ea3
somePythonProgrammer <74598401+somePythonProgrammer@users.noreply.github.com>
2021-10-07 12:43:02 +0530

    Enable Culling

--- __main__.py
+++ __main__.py
@@ -9,5 +9,5 @@ if __name__ == '__main__':
     window = Window(width=400, height=300, caption='PyCraft', resizable=True)
     glClearColor(0.5, 0.7, 1, 1)
     glEnable(GL_DEPTH_TEST)
-    # glEnable(GL_CULL_FACE)
+    glEnable(GL_CULL_FACE)
     pyglet.app.run()
