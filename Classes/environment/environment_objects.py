c35f4fceb7827c29e25de8131c060e2703881c11
somePythonProgrammer <74598401+somePythonProgrammer@users.noreply.github.com>
2021-12-02 09:33:15 +0530

    Adding daynight cycle

--- Classes/environment/environment_objects.py
+++ Classes/environment/environment_objects.py
@@ -2,6 +2,25 @@
 from pyglet.gl import *
 from opensimplex import OpenSimplex
 import random
+from logger import *
+
+# Function to load a texture
+def load_texture(filename):
+    """
+    load_texture
+
+    * Loads a texture from a file
+
+    :filename: path of the file to load
+    """
+    try:
+        tex = pyglet.image.load(filename).get_texture()
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+        return pyglet.graphics.TextureGroup(tex)
+    except:
+        warn("Texture Loader", "Failed to load texture: " + filename)
+        return None
 
 # values and noise generators
 seed = random.randint(-999999, 999999)
@@ -33,6 +52,36 @@ class Cloud:
         glPushMatrix()
         glColor3f(0.8, 0.8, 0.8)
         pyglet.graphics.draw(4, pyglet.gl.GL_QUADS,
+                             ('v3f', (X, y, Z,  X, y, z,  X, Y, z,  X, Y, Z)))
+        glColor3f(1, 1, 1)
+        glPopMatrix()
+
+class Sun:
+    def __init__(self, position):
+        """
+        class Sun
+
+        *Makes a single sun and draws it.
+
+        :x: x position
+        :z: z position
+        """
+        self.pos = position
+        self.size = [10, 10]
+        self.texture = load_texture("assets/textures/environment/sun.png")
+
+    def draw(self):
+        x = self.pos[0]
+        y = 50
+        z = self.pos[1]
+
+        X = x+self.size[0]
+        Y = y+10
+        Z = z+self.size[1]
+
+        glPushMatrix()
+        glColor3f(1, 1, 0)
+        pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, self.texture,
                              ('v3f', (X, y, Z,  X, y, z,  X, Y, z,  X, Y, Z)))
         glColor3f(1, 1, 1)
         glPopMatrix()
