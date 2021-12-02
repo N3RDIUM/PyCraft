51e9b24b09b9b3500f44e442a2920b5fd3247847
somePythonProgrammer <74598401+somePythonProgrammer@users.noreply.github.com>
2021-12-02 12:28:13 +0530

    Tree fix

--- Classes/structures/structure_base.py
+++ Classes/structures/structure_base.py
@@ -45,7 +45,7 @@ class birch_tree(StructureBase):
         import random
 
         self.structure_data["structure_type"] = "tree"
-        self.structure_data["structure_name"] = "tree"
+        self.structure_data["structure_name"] = "birch_tree"
         self.height = random.randint(5,6)
         self.leaf_height = random.randint(2,3)
         self.side = random.randint(1,2)
@@ -74,8 +74,8 @@ class birch_tree(StructureBase):
         for i in range(self.height):
             self.util.add_block("birch_log", [self.structure_data["structure_pos"]["x"], self.structure_data["structure_pos"]["y"] + i, self.structure_data["structure_pos"]["z"]], self.chunk)
             if i >= self.leaf_height:
-                for j in range(-self.side, self.side):
-                    for k in range(-self.side, self.side):
+                for j in range(-self.side, self.side+1):
+                    for k in range(-self.side, self.side+1):
                         if [j,k] != [0,0] or i == self.height - 1:
                             self.util.add_block("birch_leaves", [self.structure_data["structure_pos"]["x"]+j, self.structure_data["structure_pos"]["y"] + i, self.structure_data["structure_pos"]["z"]+k], self.chunk)
 
