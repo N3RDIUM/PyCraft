e1caa6d29be5566bf39f3d08d384add3bba48085
somePythonProgramemr <leenagajbhiye1003@gmail.com>
2021-12-07 12:13:11 +0530

    Using CDEF for better performance

--- Classes/terrain/terrain_generator_helper.pyx
+++ Classes/terrain/terrain_generator_helper.pyx
@@ -1,5 +1,8 @@
-def add_to_batch(chunk):
+cdef _add_to_batch(chunk):
     cdef object c = chunk
     for block, _ in c.blocks.items():
         if _ != None:
             chunk.blocks[block].add_to_batch_and_save()
+
+def add_to_batch(chunk):
+    _add_to_batch(chunk)
