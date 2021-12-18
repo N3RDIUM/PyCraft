0cc0e34e6bb39239c654a6d9cef808b67c8d0ed6
somePythonProgrammer <74598401+somePythonProgrammer@users.noreply.github.com>
2021-12-18 11:58:46 +0530

    surface caves

--- Classes/terrain/terrain_generator.py
+++ Classes/terrain/terrain_generator.py
@@ -31,18 +31,19 @@ class TerrainGenerator:
         """
         for x in range(-self.parent.parent.chunk_size + self.parent.position['x'], self.parent.parent.chunk_size + self.parent.position['z']):
             for z in range(-self.parent.parent.chunk_size + self.parent.position['x'], self.parent.parent.chunk_size + self.parent.position['z']):
-                
                 noiseval_grass = round(pycraft.lerp(self.noise.noise2d(x/10, z/10) * 2, self.noise.noise2d(x/100, z/100) * 10, self.noise.noise2d(x/500, z/500) * 50))
                 noiseval_dirt = 1+abs(round(pycraft.lerp(self.noise.noise2d(x/10, z/10) * 2, self.noise.noise2d(x/100, z/100) * 7, self.noise.noise2d(x/500, z/500) * 5)))                
                 noiseval_stone = 26+round(self.noise.noise2d(x/5000, z/5000) * 500)
 
-                self.parent.add_preloaded_block("Grass", (x, noiseval_grass, z))
-
+                if not self.noise.noise3d(x/10, noiseval_grass/10, z/10) > 0.4:
+                    self.parent.add_preloaded_block("Grass", (x, noiseval_grass, z))
+                
                 for y in range(noiseval_grass-noiseval_dirt, noiseval_grass):
-                    self.parent.add_preloaded_block("Dirt", (x, y, z))
+                    if not self.noise.noise3d(x/10, y/10, z/10) > 0.4:
+                        self.parent.add_preloaded_block("Dirt", (x, y, z))
 
                 for y in range(noiseval_grass-noiseval_dirt-noiseval_stone, noiseval_grass-noiseval_dirt):
-                    if not self.noise.noise3d(x/10, y/10, z/10) > 0.5:
+                    if not self.noise.noise3d(x/10, y/10, z/10) > 0.4:
                         self.parent.add_preloaded_block("Stone", (x, y, z))
 
                 self.parent.add_preloaded_block("Bedrock", (x, noiseval_grass-noiseval_dirt-noiseval_stone-1, z))
