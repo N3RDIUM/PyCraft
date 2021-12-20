76c88113c919b439d38de5ad89b7574ee14b9c2a
somePythonProgrammer <74598401+somePythonProgrammer@users.noreply.github.com>
2021-12-20 09:08:04 +0530

    adding shaders in the future

---
+++ dep_installer.bat
@@ -0,0 +1,8 @@
+python -m pip install --upgrade pip
+python -m pip install --upgrade pyglet
+python -m pip install --upgrade cython
+python -m pip install --upgrade opensimplex
+python -m pip install --upgrade PyOpenGL
+python -m pip install --upgrade PyShaders
+python -m pip install --upgrade PyOpenGL_accelerate
+python -m pip install --upgrade tqdm
