cc5420e4bf6b1c2a36faa94ce803becbb30be136
somePythonProgrammer <74598401+somePythonProgrammer@users.noreply.github.com>
2021-12-21 08:13:25 +0530

    shaders

--- __main__.py
+++ __main__.py
@@ -14,14 +14,23 @@ from pyglet.gl import *
 # inbuilt imports
 import Classes as pycraft
 from logger import *
-
-test = True
+from load_shaders import *
 
 info('main', 'Initializing PyCraft...')
+
+# Load all the shaders
+load_shaders()
+
+def use_shader(shader):
+    shader.use()
+
 if __name__ == '__main__':
     # create window
     window = pycraft.PyCraftWindow(width = 800, height = 500, resizable = True)
     
+    # use shaders
+    use_shader(shaders['default'])
+
     glClearColor(0.5, 0.7, 1, 1)
     # Run the app
     info('main', 'Running PyCraft...')
