1301204fc4e2b28736edc61e2251646a2bd38551
somePythonProgrammer <74598401+somePythonProgrammer@users.noreply.github.com>
2021-12-21 09:51:25 +0530

    shader fix

--- __main__.py
+++ __main__.py
@@ -25,11 +25,11 @@ def use_shader(shader):
     shader.use()
 
 if __name__ == '__main__':
-    # create window
-    window = pycraft.PyCraftWindow(width = 800, height = 500, resizable = True)
-    
     # use shaders
     use_shader(shaders['default'])
+
+    # create window
+    window = pycraft.PyCraftWindow(width = 800, height = 500, resizable = True)
 
     glClearColor(0.5, 0.7, 1, 1)
     # Run the app
