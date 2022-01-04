cab3f8db510ad2982444d6fa84f591f7575a7929
somePythonProgrammer <74598401+somePythonProgrammer@users.noreply.github.com>
2022-01-04 20:14:51 +0530

    Added new blocks

--- Classes/terrain/world.py
+++ Classes/terrain/world.py
@@ -88,7 +88,7 @@ class World:
         self.light_change = 0
 
         # Enable fog
-        #glEnable(GL_FOG)
+        glEnable(GL_FOG)
         glFogfv(GL_FOG_COLOR, (GLfloat * int(self.render_distance*16))(0.5, 0.69, 1.0, 10))
         glHint(GL_FOG_HINT, GL_DONT_CARE)
         glFogi(GL_FOG_MODE, GL_LINEAR)
@@ -184,6 +184,10 @@ class World:
         if self._frame % 2 == 0:
             self._process_queue_item()
 
+        # Updates the liquids
+        for i in self.liquid_types:
+            self.liquid_types[i].update()
+
         # INFGEN
         if self.parent.player.pos[0] / self.chunk_size > self.position[0] + self.infgen_threshold:
             self.add_row_x_plus()
@@ -221,10 +225,6 @@ class World:
         glClearColor(*self.sky_color, 255)
         # Lights
         glLightfv(GL_LIGHT7, GL_AMBIENT, (GLfloat * 4)(*self.light_color))
-
-        # Updates the liquids
-        for i in self.liquid_types:
-            self.liquid_types[i].update()
 
         # Draw clouds and chunks
         self.cloud_generator.draw()
