2c0f8772099b95577dbe678dcc0a88ad15cd9276
somePythonProgrammer <74598401+somePythonProgrammer@users.noreply.github.com>
2022-01-05 09:16:01 +0530

    update deps

--- dep_installer.bat
+++ dep_installer.bat
@@ -1,7 +1,7 @@
 python -m pip install --upgrade pip
 python -m pip install --upgrade pyglet
 python -m pip install --upgrade opensimplex
-python -m pip install --upgrade PyOpenGL
 python -m pip install --upgrade PyShaders
-python -m pip install --upgrade PyOpenGL_accelerate
 python -m pip install --upgrade tqdm
+python -m pip install --upgrade pyglm
+python -m pip install --upgrade ratcave
