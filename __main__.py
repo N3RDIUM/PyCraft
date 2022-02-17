f587136b6d350fbb4d68d3e54161bbe39b6d8ffc
somePythonProgrammer <74598401+somePythonProgrammer@users.noreply.github.com>
2022-02-17 20:58:02 +0530

    Key movement

--- __main__.py
+++ __main__.py
@@ -8,7 +8,7 @@
 #################################################################
 
 # imports
-from calendar import c
+import random
 import glfw
 from OpenGL.GL import *
 from OpenGL.GLU import *
@@ -25,7 +25,12 @@ renderer = TerrainRenderer(window)
 
 glEnable(GL_DEPTH_TEST)
 
-renderer.add_cube((0, 0, -5))
+for i in range(-10, 10):
+    for j in range(-10, 10):
+        x = i
+        y = random.randint(-1, 1)
+        z = j
+        renderer.add_cube((x, y, z))
 
 camrot = [0, 0]
 campos = [0, 0, 0]
@@ -44,18 +49,51 @@ def _setup_3d():
     glMatrixMode(GL_MODELVIEW)
     glLoadIdentity()
 
+def update_on_resize():
+    _setup_3d()
+    glViewport(0, 0, *get_window_size())
+
 # mainloop
 while not glfw.window_should_close(window):
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
+    update_on_resize()
+
     _setup_3d()
     glClearColor(0.0, 0.0, 0.0, 1.0)
     glColor3f(1.0, 1.0, 1.0)
-    glTranslatef(0, 0, -10)
 
+    # First person camera translation and rotation
     glRotatef(camrot[0], 1, 0, 0)
-    glRotatef(camrot[1], 0, 0, 1)
+    glRotatef(camrot[1], 0, 1, 0)
     glTranslatef(-campos[0], -campos[1], -campos[2])
+
+    # keyboard input
+    if glfw.get_key(window, glfw.KEY_ESCAPE) == glfw.PRESS:
+        glfw.set_window_should_close(window, True)
+    # wasd movement
+    if glfw.get_key(window, glfw.KEY_W) == glfw.PRESS:
+        campos[2] -= 0.1
+    if glfw.get_key(window, glfw.KEY_S) == glfw.PRESS:
+        campos[2] += 0.1
+    if glfw.get_key(window, glfw.KEY_A) == glfw.PRESS:
+        campos[0] -= 0.1
+    if glfw.get_key(window, glfw.KEY_D) == glfw.PRESS:
+        campos[0] += 0.1
+    # Space and shift
+    if glfw.get_key(window, glfw.KEY_SPACE) == glfw.PRESS:
+        campos[1] += 0.1
+    if glfw.get_key(window, glfw.KEY_LEFT_SHIFT) == glfw.PRESS:
+        campos[1] -= 0.1
+    # Arrow keys rotation
+    if glfw.get_key(window, glfw.KEY_UP) == glfw.PRESS:
+        camrot[0] -= 1
+    if glfw.get_key(window, glfw.KEY_DOWN) == glfw.PRESS:
+        camrot[0] += 1
+    if glfw.get_key(window, glfw.KEY_LEFT) == glfw.PRESS:
+        camrot[1] -= 1
+    if glfw.get_key(window, glfw.KEY_RIGHT) == glfw.PRESS:
+        camrot[1] += 1
 
     renderer.render()
 
