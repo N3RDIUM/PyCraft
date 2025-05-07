79fe28c556b05a25c01a676211293b91da992aa3
n3rdium <n3rdium@gmail.com>
2025-05-07 07:19:12 +0530

    Moving shaders to separate files

--- src/main.py
+++ src/main.py
@@ -38,6 +38,7 @@ from core.state import State
 # TODO: logging
 # TODO: organize and refactor code
 # TODO: update viewport on resize
+# TODO: load shaders like assets instead of hardcoding them
 
 data = np.random.rand(120).astype(np.float32)
 
