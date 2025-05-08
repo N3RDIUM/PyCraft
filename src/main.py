ec8ea426620770e0ec923682e394c64247a32b64
n3rdium <n3rdium@gmail.com>
2025-05-09 00:32:36 +0530

    Remove unneeded comment

--- src/main.py
+++ src/main.py
@@ -1,12 +1,6 @@
 from core.window import Window
 from core.asset_manager import AssetManager
 
-# TODO: use glm
-# TODO: logging
-# TODO: organize and refactor code
-# TODO: update viewport on resize
-# TODO: load shaders like assets instead of hardcoding them
-
 if __name__ == "__main__":
     window: Window = Window()
     asset_manager: AssetManager = AssetManager(window.state)
