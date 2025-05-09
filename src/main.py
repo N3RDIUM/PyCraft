63380f5a4df392b6a57b69aa48eabadb092dc567
n3rdium <n3rdium@gmail.com>
2025-05-09 13:49:29 +0530

    Many chunks

--- src/main.py
+++ src/main.py
@@ -2,18 +2,19 @@ from core.window import Window
 from core.asset_manager import AssetManager
 from terrain.chunk import Chunk
 from player import Player
+import threading
+import time
+
+def gen_chunks(dst, state):
+    for x in range(-dst, dst + 1):
+        for y in range(-dst, dst + 1):
+            time.sleep(1)
+            Chunk([x, 0, y], state)
 
 if __name__ == "__main__":
     window: Window = Window()
     asset_manager: AssetManager = AssetManager(window.state)
     asset_manager.load_assets()
-    chunks = []
-    
-    dst = 2
-    for x in range(-dst, dst + 1):
-        for y in range(-dst, dst + 1):
-            chunk: Chunk = Chunk([x, 0, y], window.state)
-            chunks.append(chunk)
-    
+    thread = threading.Thread(target=lambda: gen_chunks(2, window.state))
     player = Player(window.state)
     window.start_mainloop()
